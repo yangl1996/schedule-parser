f609c45497a36a3d0fe281df9c1fa9c7a078c50f
Lei Yang <yltt1234512@gmail.com>
2015-03-04 23:49:50 +0800

    completed: read in the file using correct coding

--- parser.py
+++ parser.py
@@ -1,1 +1,23 @@
-__author__ = 'yangl1996'
+# -*- coding: utf-8 -*-
+
+from html.parser import HTMLParser
+
+
+class ScheduleParser(HTMLParser):
+    def handle_starttag(self, tag, attrs):
+        print("Encountered a start tag:", tag)
+
+    def handle_endtag(self, tag):
+        print("Encountered an end tag :", tag)
+
+    def handle_data(self, data):
+        print("Encountered some data  :", data)
+
+
+file_name = 'sample.html'
+raw_file = open(file_name, 'r', encoding='utf-8')
+file_text = raw_file.read()
+
+
+parser = ScheduleParser()
+parser.feed(file_text)
