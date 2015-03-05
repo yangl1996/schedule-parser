5d0cde3bb532f6a0db78f56bef90ec3ec95878f7
Lei Yang <yltt1234512@gmail.com>
2015-03-05 23:42:21 +0800

    auto fetch html file according to username and password

--- parser.py
+++ parser.py
@@ -1,111 +1,28 @@
 # -*- coding: utf-8 -*-
-
+import requests
+import random
+import json
 from html.parser import HTMLParser
 
 
-time_count = 0
-day_count = -1
-now_class = False
-temp_data_field = []
-
-
-class_info = []
-
-
-class ScheduleParser(HTMLParser):
-    def handle_starttag(self, tag, attrs):
-        global time_count
-        global now_class
-        global day_count
-        if tag == 'tr' and (attrs[0][1] == 'course-even' or attrs[0][1] == 'course-odd'):
-            time_count += 1
-        elif tag == 'td' and len(attrs) == 2:
-            day_count += 1
-            if day_count == 8:
-                day_count = 0
-        elif tag == 'td' and len(attrs) == 3:
-            day_count += 1
-            now_class = True
-
-    def handle_endtag(self, tag):
-        global now_class
-        global temp_data_field
-        global class_info
-        if tag == 'td':
-            if now_class == True:
-                this_class = {'day': day_count, 'time': time_count, 'info': temp_data_field[:]}
-                class_info.append(this_class)
-                temp_data_field = []
-                now_class = False
-
-    def handle_data(self, data):
-        global temp_data_field
-        if now_class:
-            temp_data_field.append(data)
-
-
-file_name = input('Enter file path of HTML\n')
-try:
-    raw_file = open(file_name, 'r', encoding='utf-8')
-    file_text = raw_file.read()
-    file_text = file_text[file_text.find('<table id="classAssignment" class="course" width="100%">'):file_text.find('<script language="JavaScript" type="text/JavaScript">')]
-except:
-    print("Can't open HTML file. Exiting.")
-    exit()
-
-
-parser = ScheduleParser()
-parser.feed(file_text)
-raw_file.close()
-
-# Parsing completed
-refined_table = []
-
-for every_class in class_info:
-    class_day = every_class['day']
-    class_time = every_class['time']
-    class_name = every_class['info'][0]
-    #  0 for each week
-    #  1 for odd  week
-    # -1 for even week
-    class_type = 0
-    for all_info in every_class['info']:
-        if not all_info.find('单周') == -1:
-            class_type = 1
-            break
-        elif  not all_info.find('双周') == -1:
-            class_type = -1
-            break
-    class_test = 'N/A'
-    for all_info in every_class['info']:
-        if not all_info.find('考试') == -1:
-            class_test = all_info
-    class_location = 'N/A'
-    for all_info in every_class['info']:
-        if all_info.startswith('(') and not all_info.startswith('(备注'):
-            class_location = all_info[1:all_info.find(')')]
-    class_remark = 'N/A'
-    for all_info in every_class['info']:
-        if all_info.startswith('(备注'):
-            class_remark = all_info[1:all_info.find(')')]
-    refined_info = {'day': class_day, 'time': class_time, 'name': class_name,
-                    'type': class_type, 'exam': class_test, 'room': class_location, 'remark': class_remark}
-    refined_table.append(refined_info)
-
-
-# Refine completed
-for every_class in refined_table:
-    every_class['start'] = every_class['time']
-    every_class['end'] = every_class['time']
-    del every_class['time']
-    for another_class in refined_table:
-        if not another_class == every_class:
-            if another_class['day'] == every_class['day'] and another_class['name'] == every_class['name']:
-                if another_class['time'] < every_class['start']:
-                    every_class['start'] = another_class['time']
-                elif another_class['time'] > every_class['end']:
-                    every_class['end'] = another_class['time']
-                refined_table.remove(another_class)
+def handle_iaaa(username, password):
+    URL = 'https://iaaa.pku.edu.cn/iaaa/oauthlogin.do'
+    random_number = random.random()
+    payload = {'appid': 'syllabus',
+               'userName': str(username),
+               'password': str(password),
+               'randCode': str(random_number),
+               'redirUrl': 'http://elective.pku.edu.cn:80/elective2008/agent4Iaaa.jsp/../ssoLogin.do'}
+    session = requests.Session()
+    r = session.post(URL, data=payload)
+    result = json.loads(r.text)
+    if result['success'] is True:
+        token = result['token']
+        new_url = 'http://elective.pku.edu.cn/elective2008/ssoLogin.do' + '?rand=' + str(random_number) + '&token=' + token
+        session.get(new_url)
+        return session.get('http://elective.pku.edu.cn/elective2008/edu/pku/stu/elective/controller/electiveWork/showResults.do').text
+    else:
+        return 'LoginError'
 
 
 def get_time(class_no, start=True):
@@ -159,6 +76,114 @@ def get_time(class_no, start=True):
             return '203000'
         elif class_no == 12:
             return '213000'
+
+
+print('Initializing')
+time_count = 0
+day_count = -1
+now_class = False
+temp_data_field = []
+class_info = []
+
+
+class ScheduleParser(HTMLParser):
+    def handle_starttag(self, tag, attrs):
+        global time_count
+        global now_class
+        global day_count
+        if tag == 'tr' and (attrs[0][1] == 'course-even' or attrs[0][1] == 'course-odd'):
+            time_count += 1
+        elif tag == 'td' and len(attrs) == 2:
+            day_count += 1
+            if day_count == 8:
+                day_count = 0
+        elif tag == 'td' and len(attrs) == 3:
+            day_count += 1
+            now_class = True
+
+    def handle_endtag(self, tag):
+        global now_class
+        global temp_data_field
+        global class_info
+        if tag == 'td':
+            if now_class == True:
+                this_class = {'day': day_count, 'time': time_count, 'info': temp_data_field[:]}
+                class_info.append(this_class)
+                temp_data_field = []
+                now_class = False
+
+    def handle_data(self, data):
+        global temp_data_field
+        if now_class:
+            temp_data_field.append(data)
+
+try:
+    file_text = handle_iaaa('', '')  # enter username and password
+    if not file_text == 'LoginError':
+        file_text = file_text[file_text.find('<table id="classAssignment" class="course" width="100%">'):file_text.find('<script language="JavaScript" type="text/JavaScript">')]
+        print('Class info fetched')
+    else:
+        print("Error logging in elective system! Exiting.")
+        exit()
+except:
+    print("Fatal Error! Exiting.")
+    exit()
+
+
+parser = ScheduleParser()
+parser.feed(file_text)
+
+# Parsing completed
+refined_table = []
+
+for every_class in class_info:
+    class_day = every_class['day']
+    class_time = every_class['time']
+    class_name = every_class['info'][0]
+    #  0 for each week
+    #  1 for odd  week
+    # -1 for even week
+    class_type = 0
+    for all_info in every_class['info']:
+        if not all_info.find('单周') == -1:
+            class_type = 1
+            break
+        elif  not all_info.find('双周') == -1:
+            class_type = -1
+            break
+    class_test = 'N/A'
+    for all_info in every_class['info']:
+        if not all_info.find('考试') == -1:
+            class_test = all_info
+    class_location = 'N/A'
+    for all_info in every_class['info']:
+        if all_info.startswith('(') and not all_info.startswith('(备注'):
+            class_location = all_info[1:all_info.find(')')]
+    class_remark = 'N/A'
+    for all_info in every_class['info']:
+        if all_info.startswith('(备注'):
+            class_remark = all_info[1:all_info.find(')')]
+    refined_info = {'day': class_day, 'time': class_time, 'name': class_name,
+                    'type': class_type, 'exam': class_test, 'room': class_location, 'remark': class_remark}
+    refined_table.append(refined_info)
+
+
+# Refine completed
+for every_class in refined_table:
+    every_class['start'] = every_class['time']
+    every_class['end'] = every_class['time']
+    del every_class['time']
+    for another_class in refined_table:
+        if not another_class == every_class:
+            if another_class['day'] == every_class['day'] and another_class['name'] == every_class['name']:
+                if another_class['time'] < every_class['start']:
+                    every_class['start'] = another_class['time']
+                elif another_class['time'] > every_class['end']:
+                    every_class['end'] = another_class['time']
+                refined_table.remove(another_class)
+
+
+print('Class info HTML parsed!')
 
 
 ics_file = '''BEGIN:VCALENDAR
@@ -239,6 +264,7 @@ CREATED:20150305T080000Z
 ics_file += 'END:VCALENDAR'
 
 try:
+    print('Writing .ics file!')
     write_file = open('class.ics', 'w', encoding='utf-8')
     write_file.write(ics_file)
     write_file.close()
