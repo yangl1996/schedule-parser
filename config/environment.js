ea1d0da82202734c9b70a956984b4f8d96667138
Lei Yang <yltt1234512@gmail.com>
2016-06-16 01:23:37 +0800

    change index to a singing in page

--- config/environment.js
+++ config/environment.js
@@ -2,4 +2,7 @@ module.exports = function(app, express){
   app.use(express.static(__dirname + '/../static')); // public static files
   app.set('views', __dirname + '/../views'); // template directory
   app.set('view engine', 'pug'); // template engine
+  app.locals.pretty = true;
+  if (app.get('env') === 'development') {
+  }
 };
