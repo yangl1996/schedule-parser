9cfc3222b86afc969dd5c899084af587785fb942
Lei Yang <yltt1234512@gmail.com>
2016-06-16 00:21:54 +0800

    set up directory structure and an index page

---
+++ app.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+
+/* Load configurations and routes */
+require('./config/environment.js')(app, express);
+require('./config/routes.js')(app);
+
+/* Start the app */
+app.listen(process.env.SQUIRREL_PORT || 3000, function() {
+    console.log('Server started.');
+});
---
+++ config/environment.js
@@ -0,0 +1,5 @@
+module.exports = function(app, express){
+  app.use(express.static(__dirname + '/../static')); // public static files
+  app.set('views', __dirname + '/../views'); // template directory
+  app.set('view engine', 'pug'); // template engine
+};
