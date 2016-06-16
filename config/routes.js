35ac796d786eff5c9b5f9b57f0ab9238ee104bc1
Lei Yang <yltt1234512@gmail.com>
2016-06-16 12:41:16 +0800

    fetch captcha for user

--- config/routes.js
+++ config/routes.js
@@ -1,5 +1,24 @@
+var request = require('request')
+
 module.exports = function(app) {
+  /* display homepage */
   app.get('/', function(req, res) {
     res.render('index');
   });
+
+  /* fetch captcha and send to client */
+  app.get('/dean-captcha', function(req, res) {
+    request({
+      uri: 'http://dean.pku.edu.cn/student/yanzheng.php?act=init',
+      headers: {
+        'host': 'dean.pku.edu.cn',
+        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+        'user-agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:28.0) Gecko/20100101  Firefox/28.0',
+        'accept-language': 'zh-cn',
+        'accept-encoding': 'gzip, deflate',
+        'connection': 'keep-alive'
+      }
+    }).pipe(res);
+  });
+
 };
--- static/custom-js/signin.js
+++ static/custom-js/signin.js
@@ -1,3 +1,3 @@
 function check_img() {
-  document.getElementById('dean-captcha').src = "http://dean.pku.edu.cn/student/yanzheng.php?act=init&rand=" + Math.random();
+  document.getElementById('dean-captcha').src = '/dean-captcha';
 }
