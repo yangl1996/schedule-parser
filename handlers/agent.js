558723a9cc5f6047b1137543c104874f42a9f3f4
Lei Yang <yltt1234512@gmail.com>
2016-06-17 16:51:18 +0800

    handle loggin in and return status

--- handlers/agent.js
+++ handlers/agent.js
@@ -17,12 +17,12 @@ var trivial_header = {
 var post_header = {
   'host': 'dean.pku.edu.cn',
   'content-type': 'application/x-www-form-urlencoded',
-  'origin': 'http://dean.pku.edu.cn/student/',
+  'origin': 'http://dean.pku.edu.cn',
   //'content-length': '0',
   'connection': 'keep-alive',
   'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
   'user-agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:28.0) Gecko/20100101  Firefox/28.0',
-  'referer': 'http://dean.pku.edu.cn/student/',
+  'referer': 'http://dean.pku.edu.cn/student/index.html',
   'accept-language': 'zh-cn',
   'accept-encoding': 'gzip, deflate'
 }
@@ -36,8 +36,31 @@ function fetch_captcha(req, res) {
 
 function handle_login_form(req, res) {
   fake_header = post_header;
-  fake_header['cookie'] = req.get('cookie');
+  fake_header['cookie'] = req.get('cookie');  // pass the cookie to make captcha work
   user_id = req.body.inputID;
   user_password = req.body.inputPassword;
   user_captcha = req.body.inputCaptcha;
+  login_status = 0;  // 0 for unknown error, 1 for success, 2 for wrong identity, 3 for wrong captcha
+  request.post({
+    uri: 'http://dean.pku.edu.cn/student/authenticate.php',
+    form: {
+      'sno': user_id,
+      'password': user_password,
+      'captcha': user_captcha,
+      'submit': '%B5%C7%C2%BC'
+    },
+    headers: fake_header
+  }, function (error, response, body) {
+    // check status
+    if (body.indexOf('图形校验码错误，请重新登录') > -1) {
+      login_status = 3;
+    }
+    else if (body.indexOf('学号不存在，或者密码有误') > -1) {
+      login_status = 2;
+    }
+    else if (body.indexOf('student_index.php?PHPSESSID=') > -1) {
+      login_status = 1;
+    }
+    console.log(login_status);
+  });
 }
